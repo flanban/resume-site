204de835c602594f6dd8a2671a54c50e927f8a01
flanban <johnsfuller@gmail.com>
2014-10-06 18:22:19 -0400

    configure font paths in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # include webfont paths in asset pipeline
+  config.assets.precompile += %w( .svg .eot .woff .ttf)
+
 end
