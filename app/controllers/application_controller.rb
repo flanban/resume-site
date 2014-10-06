e3c638675239068aa0dd220e24b21f893f189131
flanban <johnsfuller@gmail.com>
2014-10-06 18:32:57 -0400

    add controller action as body class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #add body class for controller
+  before_filter lambda { @body_class = 'page' }
+
 end
