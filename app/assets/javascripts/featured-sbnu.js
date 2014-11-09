2a432ee430a9153da62bf17079e8ebd4bd83660e
flanban <johnsfuller@gmail.com>
2014-11-09 18:52:04 -0500

    merging sbnu page, wip

---
+++ app/assets/javascripts/featured-sbnu.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".main").onepage_scroll({
+     sectionContainer: "section",
+     easing: "ease",
+     animationTime: 1000,
+     pagination: true,
+     updateURL: false
+  });
+});
