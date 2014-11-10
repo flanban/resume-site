9a46979b73b4c716c2be4d7ad6a624183ed8f5da
flanban <johnsfuller@gmail.com>
2014-11-09 21:42:01 -0500

    styling main nav
    wrong branch, my bad

--- app/assets/javascripts/square-nav.js
+++ app/assets/javascripts/square-nav.js
@@ -1,6 +1,12 @@
 $( document ).ready(function() {
     $(".square-normal").click(function (e){
       e.preventDefault;
-      $(this).children('div').fadeToggle('slow');
+      $(this).children('div').fadeIn();
+    });
+    
+    $(".close-btn").click(function (e){
+      e.preventDefault;
+      e.stopPropagation();
+      $('.drop').fadeOut();
     });
 });
