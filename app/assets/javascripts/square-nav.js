16458ff663b5f9e994627e700b22f655c94d7053
flanban <johnsfuller@gmail.com>
2015-04-28 18:32:50 -0400

    memu updates

--- app/assets/javascripts/square-nav.js
+++ app/assets/javascripts/square-nav.js
@@ -1,7 +1,6 @@
 $( document ).ready(function() {
     $(".square-normal").click(function (e){
       e.preventDefault;
-      $('.drop').fadeOut(); //close all subnav b4 opening another
       $(this).children('div').fadeIn();
     });
     
