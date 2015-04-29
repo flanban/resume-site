7ec0c622da12a795656c7a07aaa92675c0033b86
flanban <johnsfuller@gmail.com>
2015-04-29 11:04:11 -0400

    hide/show big letters

--- app/assets/javascripts/square-nav.js
+++ app/assets/javascripts/square-nav.js
@@ -1,14 +1,26 @@
 $( document ).ready(function() {
-    $(".square-normal").click(function (e){
-      e.preventDefault;
-      $(this).children('div').fadeIn();
-    });
+  
+  
+  function hideLetters() {
+    $('.big-letter').fadeOut()
+  }
+  function showLetters() {
+    $('.big-letter').show()
+  }
+  
+  $(".square-normal").click(function (e){
+    e.preventDefault;
+    $(this).children('div').fadeIn();
     
-    $(".close-btn").click(function (e){
-      e.preventDefault;
-
-      // does sketchy ajjax things. why is this here?
-      e.stopPropagation();
-      $('.drop').fadeOut();
-    });
+    hideLetters()
+  });
+  
+  $(".close-btn").click(function (e){
+    e.preventDefault;
+    // does sketchy ajjax things. why did i put this here?
+    e.stopPropagation();
+    $('.drop').fadeOut();
+    
+    showLetters()
+  });
 });
